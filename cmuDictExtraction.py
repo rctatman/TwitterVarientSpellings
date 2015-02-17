6598bdabb8faa4212711ccb0fba6867a8ef2228f
Rachael Tatman <rctatman@users.noreply.github.com>
2015-02-17 10:48:13 -0800

    Create cmuDictExtraction.py
    Just a little python script to extract the 100 most frequent words, and all thier variant pronunciations, from the CMU pronouncing dictionary.

---
+++ cmuDictExtraction.py
@@ -0,0 +1,25 @@
+# creates a new file
+dicByFreq = open("dicByFreq.txt", "w")
+
+# opens existing file in same directory
+dic = open("cmudict-0.7b.txt", "r")
+freq = open("freqTop100.txt", "r")
+# this prevents us from having to go back to the beginning of the file in every loop.
+dic = dic.readlines()
+
+# Takes the word from the frequency file, then looks for it in the CMU dictionary, including variants.
+# Appends the dictionary entry to our new file.
+
+for line in freq:
+    end = line.index("\n")
+    name = line[0:end]
+    for entry in dic:
+        if entry.startswith(name.upper()+" "):
+            newline = entry
+            dicByFreq.write(newline)
+        if entry.startswith(name.upper()+"("):
+            newline = entry
+            dicByFreq.write(newline)
+
+# closes your new file with all the info you extracted
+dicByFreq.close()
